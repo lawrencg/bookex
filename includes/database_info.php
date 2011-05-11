62ec258ce519806b2652487bee28ad2c78c6c460
larry <larry@25dc2bb2-c519-32c4-a1a3-91b1f6e628e4>
2011-05-11 06:57:52 +0000

    Slick include?

---
+++ includes/database_info.php
@@ -0,0 +1,7 @@
+<?php 
+	# Database connection parameters
+	$DATABASE = "larry_test";
+	$DB_USER = "shanzha";
+	$DB_PASSWORD = "lawrence";
+	$DB_CONNECT_STRING = "host=vergil.u.washington.edu port=10450 dbname=" . $DATABASE . " user=" . $DB_USER . " password=" . $DB_PASSWORD;
+?>
