8a37e30689a1734db1b0904fe10e617db37627ac
larry <larry@25dc2bb2-c519-32c4-a1a3-91b1f6e628e4>
2011-05-11 06:57:12 +0000

    Comments and changed the database connection sessions. Did not have a pg_close() initially.

--- includes/mybooks.php
+++ includes/mybooks.php
@@ -1,20 +1,38 @@
 <?php
+	# Author: Lawrence Gabriel
+	# Email: shanzha@uw.edu
+	# Date: May 10, 2011
+	# Title: Display a users books for the BookEx web application.
+	
+	# menu.php include must be before any HTML. The PHP session can only be started before any HTML is output.
+	# Might need to change this configuration later becasue menu.php will not be the first include. 
+	# menu.php was the easiest palce to start the session globally so that bug submission could have a previous page URL.
 	include 'menu.php';
+	# Database connection parameters
+	include 'database_info.php';
 	include "search.php";
 	include 'greeting.php';
-	$DATABASE = "larry_test";
-	$DB_USER = "shanzha";
-	$DB_PASSWORD = "lawrence";
-	$DB_CONNECT_STRING = "host=vergil.u.washington.edu port=10450 dbname=" . $DATABASE . " user=" . $DB_USER . " password=" . $DB_PASSWORD;
 	
+	# Global variables
+	# Who knows what someone will try to POST
 	$bookex_id = pg_escape_string($_POST['bookexid']);
+	# Get the current UW NetID from the server via pubcookie
 	$user = $_SERVER['REMOTE_USER'];
 
+	# This POST came from the bookdetail.php page
+	# This was the most logical place to drop users after they removed one of their books.
 	if(isset($_POST['confirmdelete'])){
+		# Connect to the database
 		$dbconn = pg_connect($DB_CONNECT_STRING)
 		    or die('Could not connect: ' . pg_last_error());
 		$result = pg_query("SELECT removebook('{$bookex_id}'::integer, '{$user}'::varchar)") 
 			or die('Query failed: ' . pg_last_error()); 
+		# removebook() returns a boolean
+		# Users can only remove a book that is not currently loaned out to someone else 
+		# OR is not available for others to borrow. They must set it to available first then 
+		# remove it. This is caused by a reuse of the funciton isstillavailable(bookid) in the 
+		# removebook function. We don't want books to disappear that are accidentally removed when
+		# someone is currently borrowing it.
 		while($records = pg_fetch_array($result)) {
 			if($records[0] == f){
 				echo "<h2>ERROR: Your book could not be removed at this time. Please ensure that 
@@ -23,51 +41,73 @@
 				echo "<h2>Your book has been removed.</h2>";
 			}
 		}
+		pg_close($dbconn);
 	}
 
-	$user = $_SERVER['REMOTE_USER'];
-		$dbconn = pg_connect($DB_CONNECT_STRING)
-		    or die('Could not connect: ' . pg_last_error());
-		$books = pg_query("SELECT * FROM getmybooks('{$user}') VALUES (bookid int, owner varchar, 
-			date timestamp, title varchar, borrower varchar, available varchar, transstatus varchar)") 
-			or die('Query failed: ' . pg_last_error()); 
-		$firsttime = true;
-		while($records = pg_fetch_array($books)) {
-			if($records[4] == "")
-				$borrow = '&nbsp;';
-			else 
-				$borrow = $records[4];
-			if ($firsttime) {
-				echo "<table border='2px'>";
-				echo "<th>Title</th><th>Borrower</th><th>Due</th><th>Status</th>";
-				echo "<tr><td><a href='bookdetail.php?id={$records[0]}'>{$records[3]}</td>
-				<td>{$borrow}</td>";
-				if($records[6] == ''){
-					echo "<td>&nbsp;</td><td>";
-					echo $records[5];
-				} else { 
-					echo "<td>".date("F j, Y")."</td><td>";
-					echo $records[6];
-				}
-				echo "</td></tr>";
-				$firsttime = false;
-			} else {
-				echo "<tr><td><a href='bookdetail.php?id={$records[0]}'>{$records[3]}</td>
-				<td>{$borrow}</td>";
-				if($records[6] == ''){
-					echo "<td>&nbsp;</td><td>";
-					echo $records[5];
-				} else { 
-					echo "<td>".date("F j, Y")."</td><td>";
-					echo $records[6];
-				}
-				echo "</td></tr>";
+	# Connect to database
+	$dbconn = pg_connect($DB_CONNECT_STRING)
+	    or die('Could not connect: ' . pg_last_error());
+	$books = pg_query("SELECT * FROM getmybooks('{$user}') VALUES (bookid int, owner varchar, 
+		date timestamp, title varchar, borrower varchar, available varchar, transstatus varchar)") 
+		or die('Query failed: ' . pg_last_error()); 
+	# Used a a flag to see if there are actually any books to display. Create a header if yes, and at the end
+	# only close the table if we created one.
+	$firsttime = true;
+	# Reuslts are displayed in reverse chronological order. The most newly added books are displayed first. 
+	# Hopefully this was a good choice over alphabetical. Good: if a user has 500 books, most likely they are going to
+	# want to see the books they most recently added. Bad: The have 500 books and want to find a book with a title 
+	# that starts with "B"
+	while($records = pg_fetch_array($books)) {
+		# Create a blank space for the borrower column if the book is not currently loaned
+		if($records[4] == "")
+			$borrow = '&nbsp;';
+		else 
+			$borrow = $records[4];
+		if ($firsttime) {
+			echo "<table border='2px'>";
+			echo "<th>Title</th><th>Borrower</th><th>Due</th><th>Status</th>";
+			# Book title is a link to book details for that book
+			echo "<tr><td><a href='bookdetail.php?id={$records[0]}'>{$records[3]}</td>
+			<td>{$borrow}</td>";
+			# Decides what status to display. For loaned books we need to display the transaction status,
+			# for books that are not loaned out, we need to show the availability status.
+			if($records[6] == ''){
+				echo "<td>&nbsp;</td><td>";
+				echo $records[5];
+			} else { 
+				# The database does not store "Due" dates yet. This is just a place holder
+				echo "<td>".date("F j, Y")."</td><td>";
+				echo $records[6];
 			}
-		}		
-		if(!$firsttime){
-			echo "</table><br />";
+			echo "</td></tr>";
+			# Found one book, need to close the table tag at the end.
+			$firsttime = false;
 		} else {
-			echo "<p><i>&nbsp;&nbsp;&nbsp;You do not currently have any books added to your account.</i></p>"; 
+			# Book title is a link to book details for that book
+			echo "<tr><td><a href='bookdetail.php?id={$records[0]}'>{$records[3]}</td>
+			<td>{$borrow}</td>";
+			# Decides what status to display. For loaned books we need to display the transaction status,
+			# for books that are not loaned out, we need to show the availability status.
+			if($records[6] == ''){
+				echo "<td>&nbsp;</td><td>";
+				echo $records[5];
+			} else { 
+				# The database does not store "Due" dates yet. This is just a place holder
+				echo "<td>".date("F j, Y")."</td><td>";
+				echo $records[6];
+			}
+			echo "</td></tr>";
 		}
-		echo "<a href='addbook.php'>Add a Book</a>\n";
+	}
+	# Close database connection
+	pg_close($dbconn);	
+	# Close the table if we added a book.	
+	if(!$firsttime){
+		echo "</table><br />";
+	# User has no books associated with their BookEx account
+	} else {
+		echo "<p><i>&nbsp;&nbsp;&nbsp;You do not currently have any books added to your account.</i></p>"; 
+	}
+	# Here is an option for the user to do
+	echo "<a href='addbook.php'>Add a Book</a>\n";
 ?>
