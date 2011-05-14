cdd92f081c33fbdc33f5856e1ed4ef2e5fdd13d3
sopheap <sopheap@25dc2bb2-c519-32c4-a1a3-91b1f6e628e4>
2011-05-14 21:51:56 +0000

    bookdetails initial

---
+++ includes/bookdetails_0_header.php
@@ -0,0 +1,11 @@
+<!-- SPLIT start bookdetails_0_header.php -->
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN"
+ "http://www.w3.org/TR/html4/strict.dtd" author and creator: Jessica Pardee>
+<html>
+	<head>
+		<title>My Book Details</title>
+		<link rel="stylesheet" href="styles/main.css"/>
+	</head>
+	<body>
+		<div id="pagecontainer">
+		<!-- SPLIT end bookdetails_0_header.php -->
---
+++ includes/bookdetails_1_contentarea.php
@@ -0,0 +1,16 @@
+<!-- SPLIT start bookdetails_1_contentarea.php -->
+			<div id="page">
+				<div class="pageTitle">Book Details</div>
+				<div class="pageSubTitle">Book Title here</div>
+				<div id="notification" class="show hide" >Message is here</div>
+				<div id="maincontent">
+					<div id="" class="contentarea">
+						<div class="leftContent">
+							<div id="bookImageContent">
+								<div id="bookImagePhoto"></div>
+								<button class="smallbtn">Upload Photo</button>
+								<button class="actionButton">Edit</button>
+							</div>
+						</div>						
+						<div class="rightContent contentarea">
+				<!-- SPLIT end bookdetails_1_contentarea.php -->
