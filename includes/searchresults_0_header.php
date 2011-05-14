93446dbecf05adaf5d5b212c8d1b13200602b88f
jessica <jessica@25dc2bb2-c519-32c4-a1a3-91b1f6e628e4>
2011-05-14 23:36:16 +0000

    cuts for the php of html, only took table from the peoplesearchresults, and used the entire page of booksearchresults

---
+++ includes/searchresults_0_header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN"
+ "http://www.w3.org/TR/html4/strict.dtd" author and creator: Jessica Pardee>
+<html>
+<head>
+<title>Book Search Results</title>
+<!-- <link rel="stylesheet" href="main.css"/> -->
+<link rel="stylesheet" href="styles/main.css"/>
+
+</head>
+<body>
+<div id="pagecontainer">
---
+++ includes/searchresults_1_contentarea.php
@@ -0,0 +1,7 @@
+	<div id="page">
+		<div class="pageTitle">Search Results for "Introduction to JavaScript"</div>
+		<div id="notification" class="show hide" >Message is here</div>
+		<div id="maincontent">
+			<div id="searchresultsarea" class="contentarea">
+				<div id="searchresults">
+					
