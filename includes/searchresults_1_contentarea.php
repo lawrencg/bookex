e420b3a689f7e0882467133346165c7188152651
larry <larry@25dc2bb2-c519-32c4-a1a3-91b1f6e628e4>
2011-05-15 10:41:15 +0000

    removed the status div

--- includes/searchresults_1_contentarea.php
+++ includes/searchresults_1_contentarea.php
@@ -1,6 +1,5 @@
 	<div id="page">
 		<div class="pageTitle">Search Results</div>
-		<div id="notification" class="show" >Message is here</div>
 		<div id="maincontent">
 			<div id="searchresultsarea" class="contentarea">
 				<div id="searchresults">
