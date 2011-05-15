58d524ddc93543173f56abf44b2195b85552fe87
larry <larry@25dc2bb2-c519-32c4-a1a3-91b1f6e628e4>
2011-05-15 13:36:00 +0000

    Changed to a clean database.

--- includes/database_info.php
+++ includes/database_info.php
@@ -5,9 +5,9 @@
 		# Title: This is a single point of change for the BookEx web applicaton database.
 
 		# Database connection parameters
-		$DATABASE = "larry_test";
-		$DB_USER = "shanzha";
-		$DB_PASSWORD = "lawrence";
+		$DATABASE = "bookex";
+		$DB_USER = "uw";
+		$DB_PASSWORD = "poop13!13";
 		$DB_CONNECT_STRING = "host=vergil.u.washington.edu port=10450 dbname=" . $DATABASE . " user=" . $DB_USER . " password=" . $DB_PASSWORD;
 		
 		# Connect to database
