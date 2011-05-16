105ebbaef27bc158b80aecddbfe9d154a5cd0741
larry <larry@25dc2bb2-c519-32c4-a1a3-91b1f6e628e4>
2011-05-16 17:03:25 +0000

    added terms link

--- includes/sitefooter.php
+++ includes/sitefooter.php
@@ -2,7 +2,7 @@
 	<div id="footer">
 		<ul id="footernavigation">
 			<li id="privacy"><a href="#">Privacy</a></li>
-			<li id="terms"><a href="#">Terms</a></li>
+			<li id="terms"><a href="http://www.bookex.info/terms.html">Terms</a></li>
 			<li id="contact"><a href="#">Contact</a></li>
 			<li id="help"><a href="#">Help</a></li>
 			<li id="ourstory"><a href="http://www.bookex.info/ourstory.html">Our Story</a></li>
