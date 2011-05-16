5237a199cf58b50c9e3e741f640cf36bae74a7f8
larry <larry@25dc2bb2-c519-32c4-a1a3-91b1f6e628e4>
2011-05-16 17:13:37 +0000

    included site footer instead of hard coding.

--- registration.php
+++ registration.php
@@ -93,18 +93,6 @@
 				</div>
 			</div>
 		</div>
-	<div id="footer">
-		<ul id="footernavigation">
-			<li id="privacy"><a href="#">Privacy</a></li>
-			<li id="terms"><a href="http://www.bookex.info/terms.html">Terms</a></li>
-			<li id="contact"><a href="#">Contact</a></li>
-			<li id="help"><a href="#">Help</a></li>
-			<li id="ourstory"><a href="http://www.bookex.info/ourstory.html">Our Story</a></li>
-		</ul>
-		<div id="copyrightstatement">
-				&copy; 2011 All Rights Reserved by <a href="http://www.bookex.info">BookEX</a>
-		</div>
-	</div>
-	</div>	
-	</body>
-</html>
+<?php 
+	include 'includes/sitefooter.php';
+?>
