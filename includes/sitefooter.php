327ec873ed4554e09344914824176a3df20e9759
larry <larry@25dc2bb2-c519-32c4-a1a3-91b1f6e628e4>
2011-05-16 17:23:24 +0000

    footer update

--- includes/sitefooter.php
+++ includes/sitefooter.php
@@ -6,6 +6,7 @@
 	 */
 ?>
 	<div id="footer">
+		<br />
 		<ul id="footernavigation">
 			<li id="privacy"><a href="#">Privacy</a></li>
 			<li id="terms"><a href="http://www.bookex.info/terms.html">Terms</a></li>
@@ -13,6 +14,7 @@
 			<li id="help"><a href="#">Help</a></li>
 			<li id="ourstory"><a href="http://www.bookex.info/ourstory.html">Our Story</a></li>
 		</ul>
+		<br />
 		<div id="copyrightstatement">
 				&copy; 2011 All Rights Reserved by <a href="http://www.bookex.info">BookEX</a>
 		</div>
