d626012edf4f335409cc2685e6f3731bd3068650
larry <larry@25dc2bb2-c519-32c4-a1a3-91b1f6e628e4>
2011-05-16 16:35:35 +0000

    footer is not an include. need to change links separately

--- registration.php
+++ registration.php
@@ -94,11 +94,11 @@
 		</div>
 		<div id="footer">
 			<ul id="footernavigation">
-				<li id="privacy"><a href="/privacy">Privacy</a></li>
-				<li id="terms"><a href="/terms">Terms</a></li>
-				<li id="contact"><a href="/contact">Contact</a></li>
-				<li id="help"><a href="/help">Help</a></li>
-				<li id="ourstory"><a href="/ourstory">Our Story</a></li>
+				<li id="privacy"><a href="#">Privacy</a></li>
+				<li id="terms"><a href="#">Terms</a></li>
+				<li id="contact"><a href="#">Contact</a></li>
+				<li id="help"><a href="#">Help</a></li>
+				<li id="ourstory"><a href="http://www.bookex.info/ourstory.html">Our Story</a></li>
 			</ul>
 			<div id="copyrightstatement">
 					&copy; 2011 All Rights Reserved by BookEX
