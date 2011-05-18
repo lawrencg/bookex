2c7ba1e2cbedbca5757da80b03bec1e55af080fc
larry <larry@25dc2bb2-c519-32c4-a1a3-91b1f6e628e4>
2011-05-18 03:39:06 +0000

    closing tags...

--- includes/siteheader.php
+++ includes/siteheader.php
@@ -3,10 +3,10 @@
 	<meta name="author" content="Sopheap Kun" />
 	<meta name="author" content="Michael Cheung" />
 	<meta name="author" content="Lawrence Gabriel" />
-	<meta name="affiliation" content="University of Washington Information Schoool">
+	<meta name="affiliation" content="University of Washington Information Schoool" />
 	<meta name="revised" content="<?php echo date ("F d, Y G:i:s \(T\)", filemtime('/nfs/giovanni11/dw21/d77'.$_SERVER['REQUEST_URI'])); ?>" />
-	<meta name="description" content="BookEx Project, Capstone 2011">
-	<meta name="keywords" content="exchange, books, textbooks, University of Washington, Information Schoool, BookEx, students">
+	<meta name="description" content="BookEx Project, Capstone 2011" />
+	<meta name="keywords" content="exchange, books, textbooks, University of Washington, Information Schoool, BookEx, students" />
 	
 	<meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
 </head>
