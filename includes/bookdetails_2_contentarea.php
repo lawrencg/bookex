df6fcfc1243e08e5dc311f5c085a344586672ef3
larry <larry@25dc2bb2-c519-32c4-a1a3-91b1f6e628e4>
2011-05-20 00:24:04 +0000

    validated

--- includes/bookdetails_2_contentarea.php
+++ includes/bookdetails_2_contentarea.php
@@ -1,7 +1,5 @@
-<!-- SPLIT start bookdetails_2_contentarea.php -->							
 							</div>						
 						<div class="clear"></div>
 					</div>
 				</div>
 			</div>
-				<!-- SPLIT end bookdetails_2_contentarea.php -->
