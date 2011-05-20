9c960c50f00b89726453cb38202ae321afefab86
larry <larry@25dc2bb2-c519-32c4-a1a3-91b1f6e628e4>
2011-05-20 00:28:22 +0000

    validated

--- bookdetails.php
+++ bookdetails.php
@@ -115,7 +115,7 @@
 			<div><label>ISBN-13:</label><div>{$isbn13}</div></div>			
 			<div><label>Course:</label><div></div></div>			
 			<div><label>Condition:</label><div><select name='dropdown' disabled='disabled'><option value='{$cond}' selected='selected'>{$cond}</option></select></div></div>	
-			<div><label>Description:</label><div class='bookDescription'><textarea cols='40' rows='5' id='frame' name='description' style='vertical-align:text-top;' virtual disabled='disabled'>{$note}</textarea></div></div>
+			<div><label>Description:</label><div class='bookDescription'><textarea cols='40' rows='5' id='frame' name='description' style='vertical-align:text-top;' disabled='disabled'>{$note}</textarea></div></div>
 			<div><label>Available for loan?:</label><div><input type='checkbox' id='box' name='box' checked='{$status}' disabled='disabled' /></div></div>" ;
 			
 		# Security feature. Check to see if the owner is the UW NetID that is logged in.
